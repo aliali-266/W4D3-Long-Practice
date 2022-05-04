ed230eebdf39cb16683b53a6681f8a2463f79fd6
Ali Ali <aliali_266@yahoo.com>
2022-05-05 02:24:57 +0300

    Rest of the problems

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,11 +1,31 @@
 // Your code here
+class PartyPlanner {
+  constructor(guestList) {
+    this.guestList = [];
+  }
 
+  addToGuestList(name) {
+    this.guestList.push(name);
+    return this.guestList;
+  }
+
+  throwParty() {
+    if (this.guestList.length === 0) {
+      return "Gotta add people to the guest list";
+    } else {
+      return `Welcome to the party ${this.guestList.join(" and ")}`;
+    }
+  }
+}
+
+// class PartyPlanner{
+// 	constructor(guestList)
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = PartyPlanner;
+  module.exports = PartyPlanner;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
--- problems/11-bind-set-time-out.js
+++ problems/11-bind-set-time-out.js
@@ -1,7 +1,8 @@
 function boundFuncTimer(obj, func, delay) {
   // Your code here
-}
+ setTimeout(() => func.bind(obj)(),delay
 
+ )}
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
